4351a641dade5803cf566e4644774c578c3a6055
Crumbler <25861574+Crumbler@users.noreply.github.com>
2024-11-19 10:24:40 +0100

    Adjust tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,11 +4,10 @@ export default defineConfig({
   target: 'es2017',
   format: ['esm'],
   entry: ['src/scripts/main.ts'],
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   clean: true,
   dts: false,
   platform: 'browser',
-  watch: 'src/scripts',
   outDir: 'src/out',
 });
